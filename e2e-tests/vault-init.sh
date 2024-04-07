334af85c53c841132f6ffd9c8cb542472772ad86
Martin Losinský <losinsky@U7511-ML.localdomain>
2024-04-07 18:50:52 +0200

    E2E test CI timeout, vault-init.sh set executable

--- e2e-tests/vault-init.sh
+++ e2e-tests/vault-init.sh
@@ -5,8 +5,7 @@ apt-get -qq update >/dev/null
 apt-get -qq upgrade >/dev/null
 apt-get install -y curl -qq >/dev/null
 
-until curl -s -f -o /dev/null "http://keycloak:8080/realms/test"
-do
+until curl -s -f -o /dev/null "http://keycloak:8080/realms/test"; do
   echo 'Waiting for Keycloak to start...'
   sleep 5
 done
@@ -18,11 +17,10 @@ body='
   "description": "Login with JWT from Keycloak"
 }
 '
-echo $body | curl --request POST http://vault:8200/v1/sys/auth/jwt \
-    --header "Content-Type: application/json" \
-    --header "X-Vault-Token: root" \
-    --data @-
-    
+echo "$body" | curl --request POST http://vault:8200/v1/sys/auth/jwt \
+  --header "Content-Type: application/json" \
+  --header "X-Vault-Token: root" \
+  --data @-
 
 echo 'Configure vault JWT auth method to Keycloak'
 body='
@@ -31,10 +29,10 @@ body='
   "default_role": "demo"
 }
 '
-echo $body | curl --request POST http://vault:8200/v1/auth/jwt/config \
-   --header "Content-Type: application/json" \
-   --header "X-Vault-Token: root" \
-   --data @-
+echo "$body" | curl --request POST http://vault:8200/v1/auth/jwt/config \
+  --header "Content-Type: application/json" \
+  --header "X-Vault-Token: root" \
+  --data @-
 
 echo 'Create a named role'
 body='
@@ -47,7 +45,7 @@ body='
   "user_claim": "vault_user"
 }
 '
-echo $body | curl --request POST http://vault:8200/v1/auth/jwt/role/test \
-   --header "Content-Type: application/json" \
-   --header "X-Vault-Token: root" \
-   --data @-
+echo "$body" | curl --request POST http://vault:8200/v1/auth/jwt/role/test \
+  --header "Content-Type: application/json" \
+  --header "X-Vault-Token: root" \
+  --data @-
